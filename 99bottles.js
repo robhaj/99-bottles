7d4eb726926b1f0821d16343da6314f8b64e1d3d
robhaj <hajek.rob@gmail.com>
2015-07-11 11:17:01 -0600

    Fixed grammar, punctuation, and last line in 99bottles.js

--- 99bottles.js
+++ 99bottles.js
@@ -2,9 +2,12 @@ var beer, wall, bottles, take
 
 beer = 99
 bottles = "bottles of beer"
-wall = "on the wall"
+wall = "on the wall, "
+pwall = "on the wall. "
 take = ". Take one down, pass it around. "
 
-for (var beer = 99; beer > 0; beer--) {
-  console.log(beer + " " + bottles + " "  + wall + ", " + beer + " " + bottles + take + (beer - 1) + " " + bottles + " " + wall + ".")
+for (var beer = 99; beer > 2; beer--) {
+  console.log(beer + " " + bottles + " "  + wall + beer + " " + bottles + take + (beer - 1) + " " + bottles + " " + pwall);
 }
+  console.log(beer + " " + bottles + " "  + wall + beer + " " + bottles + take + (beer - 1) + " bottle of beer" + " " + pwall);
+  console.log("No more " + bottles + " " + wall + "no more " + bottles + ". Go to the store and buy some more, " + 99 + " " + bottles + " " + pwall);
