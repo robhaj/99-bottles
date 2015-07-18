7c386b9b99e43679386b9f694ccdf6398b566415
Robert Hajek <hajek.rob@gmail.com>
2015-07-18 11:07:35 -0500

    fixed second to last line (1 bottle of beer)

--- 99bottles.js
+++ 99bottles.js
@@ -10,4 +10,5 @@ for (var beer = 99; beer > 2; beer--) {
   console.log(beer + " " + bottles + " "  + wall + beer + " " + bottles + take + (beer - 1) + " " + bottles + " " + pwall);
 }
   console.log(beer + " " + bottles + " "  + wall + beer + " " + bottles + take + (beer - 1) + " bottle of beer" + " " + pwall);
+  console.log("1 bottle of beer on the wall, 1 bottle of beer" + take + " No more bottles of beer on the wall.");
   console.log("No more " + bottles + " " + wall + "no more " + bottles + ". Go to the store and buy some more, " + 99 + " " + bottles + " " + pwall);
